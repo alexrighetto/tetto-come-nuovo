23001937094aaf98f2c1d42e2731b776bce9ea78
alexrighetto <alexrighetto@gmail.com>
2017-09-10 00:13:44 +0200

    aggiunto supporto watch

--- components/coffee/tagline.coffee
+++ components/coffee/tagline.coffee
@@ -1,5 +1,5 @@
 $ = require 'jquery'
 
-do fill = (item = 'The most creative minds in Art') ->
+do fill = (item = 'Fuck') ->
   $('.tagline').append "#{item}"
 fill
--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,11 @@ var gulp    = require('gulp'),
     coffee  = require('gulp-coffee'),
     /* permette di richiedere librerie js nei files js */
     browserify = require('gulp-browserify'),
+    /* permette di richiedere librerie js nei files js */
+    compass = require('gulp-compass'),
     /* permette di concatenare i files javascript in un unico singolo file */
     concat  = require('gulp-concat');
+    
 
 /* È utile indicare tramite array le sorgenti dei file. L'asterisco inoltre significa: 'tutti i file' */
 var coffeeSources = ['components/coffee/*.coffee'];
@@ -13,8 +16,11 @@ var coffeeSources = ['components/coffee/*.coffee'];
 /* Indico tramite array i files js, allo scopo di comprimerli in un singolo. È importante l'ordine */
 var jsSources = [
     'components/scripts/tagline.js',
-    'components/scripts/template.js',
+    'components/scripts/template.js'
 ];
+
+/* Indico tramite array i files sass */
+var sassSources = ['components/sass/style.scss'];
 
 /*
 Questa è una task di testing per verificare il corretto funzionamento del file gulpfile.js. È possibile rimuoverla in qualsiasi momento.
@@ -46,3 +52,30 @@ gulp.task('js', function () {
         .pipe(gulp.dest('builds/development/js'));
     gutil.log('Eseguito js');
 });
+
+/*
+Questa task serve per unire in un unico file i css ed interpretare SASS.
+*/
+gulp.task('compass', function () {
+    gulp.src(sassSources)
+        /* il file style.css viene trasformato attraerso compass */
+        .pipe(compass({
+            /* questa linea è molto importante */
+            css: 'builds/development/css',
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            style: 'expanded'
+        }))
+        
+    .pipe(gulp.dest('builds/development/css'));
+    gutil.log('Eseguito sass');
+});
+
+gulp.task('watch', function () {
+    gulp.watch(coffeeSources, ['coffee']);
+    gulp.watch(jsSources, ['js']);
+    gulp.watch('components/sass/*.scss', ['compass']);
+});
+
+gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+
